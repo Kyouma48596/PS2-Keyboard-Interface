88c7b9ac7fe8f889a839b03e559984536b492e26
Kyouma48596 <56397478+Kyouma48596@users.noreply.github.com>
2021-07-23 17:48:24 +0530

    Revised individual alphabet files and provided COE generator script

---
+++ alphabet/script.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+
+
+alph = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+for iter in range(26):
+	img = cv.imread(alph[iter]+".jpg", 0)
+
+	_,img = cv.threshold(img, 200, 255, cv.THRESH_BINARY_INV)
+
+	coe_file = open(alph[iter]+".coe", 'w')
+	coe_file.write("memory_initialization_radix=16;\n")
+	coe_file.write("memory_initialization_vector=\n")
+	for i in range(32):
+		for j in range(32):
+			x = "1" if img[i,j]==255 else "0"
+			if (j%4==3):
+				if(i==31 and j==31):
+					coe_file.write(x+";")
+				else:
+					coe_file.write(x+", ")
+			else:
+				coe_file.write(x)
+
+
